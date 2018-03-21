630e7d2ce8d45890e23428b374adcfdd1c61c8b7
Tomasz Pach <tomaszpach@icloud.com>
2018-03-21 13:38:16 +0100

    MuiThemeProvider error fix

--- src/index.js
+++ src/index.js
@@ -1,15 +1,18 @@
 import React from 'react'
-import { render } from 'react-dom'
-import { Provider } from 'react-redux'
-import { createStore } from 'redux'
+import {render} from 'react-dom'
+import {Provider} from 'react-redux'
+import {createStore} from 'redux'
 import rootReducer from './reducers'
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import App from './components/App'
 
 const store = createStore(rootReducer);
 
 render(
     <Provider store={store}>
-        <App />
+        <MuiThemeProvider>
+            <App/>
+        </MuiThemeProvider>
     </Provider>,
     document.getElementById('root')
 );
