ad069eca78d1a89ff731b709a7ef45adf13ed9ef
Tomasz Pach <tomaszpach@icloud.com>
2018-03-21 09:34:44 +0100

    cleanup addExpense

--- src/containers/AddExpense.js
+++ src/containers/AddExpense.js
@@ -4,38 +4,32 @@ import {addExpense2, summary, updatePayback} from '../actions'
 import TextField from 'material-ui/TextField';
 import RaisedButton from 'material-ui/RaisedButton';
 
-// import TextFieldE from '../components/TextField';
-
 export class addExpense extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
             costValue: '',
             whoValue: '',
-            expense: {
-                item: 'Pizza',
-                // cost: '30',
-                // who: 'Person',
-                // amount: '1'
-            }
+            expense: {}
         };
     }
 
     handleTextFieldChange(e, field) {
-        if (field === 'cost') {
-            this.setState({
-                costValue: e.target.value
-            });
-        } else {
-            this.setState({
-                whoValue: e.target.value
-            });
+        switch (field) {
+            case 'cost':
+                this.setState({
+                    costValue: e.target.value
+                });
+                break;
+            case 'who':
+                this.setState({
+                    whoValue: e.target.value
+                });
+                break;
+            default:
+                break;
         }
     }
-
-    // componentDidMount() {
-    //     this.costInput.focus()
-    // }
 
     updateExpense() {
         this.props.addExpense2(this.state.expense);
@@ -43,11 +37,9 @@ export class addExpense extends React.Component {
         this.props.updatePayback();
     }
 
-    componentDidUpdate() {
-        console.log(this.state.costValue);
-    }
-
-    // let sPassword = this.refs.password.input.value;
+    // componentDidUpdate() {
+    //     console.log(this.state.costValue);
+    // }
 
 
     render() {
@@ -58,33 +50,21 @@ export class addExpense extends React.Component {
                 <form
                     onSubmit={e => {
                         e.preventDefault();
-                        // if (!item.value.trim()) {
-                        //     return
-                        // }
                         this.setState({
                             expense: {
-                                // item: this.textInput.value || 'Wydatek',
                                 cost: this.state.costValue || 0,
                                 who: this.state.whoValue || `Użytkownik ${this.props.expenses.expenseDetails.length + 1}`,
                             }
                         }, () => {
                             this.updateExpense();
                         });
-                        // this.costInput.value = '';
                         this.setState({
                             costValue: '',
                             whoValue: ''
                         });
-                        // this.state.costValue = '';
-                        // this.state.whoValue = '';
-                        // cost.value = '';
-                        // who.value = '';
                         cost.focus();
                     }}
                 >
-                    {/*<input placeholder='Wydatek' ref={(node) => { this.textInput = node; }} />*/}
-
-
                     <TextField
                         ref={node => cost = node}
                         autoFocus={true}
@@ -94,26 +74,15 @@ export class addExpense extends React.Component {
                         hintText={'150'}
                         floatingLabelText={'Podaj koszt*'}
                     />
-
                     <TextField
-                        // ref={node => who = node}
                         autoFocus={false}
                         type="text"
                         value={this.state.whoValue}
-                        onChange={(event) => this.handleTextFieldChange(event, 'imię')}
+                        onChange={(event) => this.handleTextFieldChange(event, 'who')}
                         hintText={'Tomek'}
                         floatingLabelText={'Podaj imię'}
                     />
-
-                    <RaisedButton type="submit" label="Dodaj wydatek" primary={true} />
-
-
-                    {/*<TextFieldE inputRef={(el) => this.btnRef = el} onChange={this.handleTextFieldChange} hintText={'150'} floatingLabelText={'Podaj koszt (bez waluty)'} autoFocus={true}  />*/}
-                    {/*<input placeholder='Koszt' ref={(node) => { this.costInput = node; }} />*/}
-                    {/*<input placeholder='Kto' ref={node => who = node} />*/}
-                    {/*<button type="submit">*/}
-                        {/*Dodaj wydatek*/}
-                    {/*</button>*/}
+                    <RaisedButton type="submit" label="Dodaj wydatek" primary={true}/>
                 </form>
             </div>
         )
@@ -121,7 +90,6 @@ export class addExpense extends React.Component {
 }
 
 function mapStateToProps(state) {
-    // console.log('state', state);
     return {
         expenses: state.expenses || [],
         summary: state.summary || {}
